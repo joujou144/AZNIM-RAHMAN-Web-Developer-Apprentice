59908b7dd5550268ed8addc63c30aa738cb85b1b
aznim <aznimr@protonmail.com>
2022-07-28 00:47:56 +0800

    updates

--- reveal.js
+++ reveal.js
@@ -5,11 +5,12 @@ ScrollReveal({
   delay: 300
 })
 
-ScrollReveal().reveal('.hero-bg, .intro-bio .page-nav', { delay: 100, origin: 'left'})
-ScrollReveal().reveal('.nav-home-menu li', { delay: 100, origin: 'top', interval: 200 })
-ScrollReveal().reveal('.intro-bio .page-nav', { delay: 200, origin: 'bottom'})
+ScrollReveal().reveal('.hero-bg, .intro-bio .page-nav', { delay: 200, origin: 'left'})
+ScrollReveal().reveal('.nav-home-menu li', { delay: 200, origin: 'top', interval: 200 })
+ScrollReveal().reveal('.intro-bio .page-nav', { delay: 300, origin: 'bottom'})
 ScrollReveal().reveal('.featured .order-two', { delay: 300, origin: 'right'})
-ScrollReveal().reveal('.featured .order-left', { delay: 350, origin: 'left'})
+ScrollReveal().reveal('.featured .order-left', { delay: 300, origin: 'left'})
 ScrollReveal().reveal('.featured .card-content', { delay: 400, origin: 'bottom'})
+
 ScrollReveal().reveal('.full-bio .bio-text', { delay: 200, origin: 'top'})
-ScrollReveal().reveal('.full-bio .bio-img', { delay: 200, origin: 'bottom'})
+ScrollReveal().reveal('.full-bio .bio-img', { delay: 300, origin: 'bottom'})
