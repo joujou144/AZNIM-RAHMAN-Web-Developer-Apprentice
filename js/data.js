4f34de2864e7375d1dca665707c222c35e3f4c13
aznim <aznimr@protonmail.com>
2022-07-30 00:47:23 +0800

    updates

--- js/data.js
+++ js/data.js
@@ -1,67 +1,81 @@
 const projectData = [
   {
     title: "Tech Investment",
-    demo: "https://joujou144.github.io/silverback/",
-    source: "https://github.com/joujou144/silverback",
+    links: [
+      '<a href="https://github.com/joujou144/silverback" target="_blank">source code</a>',
+      '<a href="https://joujou144.github.io/silverback/" target="_blank">view demo</a>'
+    ],   
     image: "./images/portfolio/silverback.png",
     desc: "A two-page interactive website with simple animation for a potential tech investment client.",
-    html: "HTML",
-    css: "CSS",
-    js: "JavaScript",
+    tags: ["HTML", "CSS", "JavaScript"]
   },
   {
     title: "Personal Dahsboard",
-    demo: "https://joujou144.github.io/personal-dashboard/",
-    source: "https://github.com/joujou144/personal-dashboard",
+    links: [
+      '<a href="https://github.com/joujou144/personal-dashboard" target="_blank">source code</a>',
+      '<a href="https://joujou144.github.io/personal-dashboard/" target="_blank">view demo</a>'
+    ],
     image: "./images/portfolio/Personal-Momentum-Dahsboard4.png",
     desc: "Page is rendered dynamically using crypto API, Unsplash images API, local time stamp and geolocation.",
-    html: "HTML",
-    css: "CSS",
-    api: "API",
-    js: "JavaScript",
+    tags: ["HTML", "CSS", "API", "JavaScript"]
   },
   {
     title: 'My Portfolio',
-    demo: "https://github.com/joujou144/AZNIMRAHMAN-Web-Dev-Apprentice",
-    source: "https://github.com/joujou144/AZNIMRAHMAN-Web-Dev-Apprentice",
+    links: [
+      '<a href="https://github.com/joujou144/AZNIMRAHMAN-Web-Dev-Apprentice" target="_blank">source code</a>'
+    ],
     image: "./images/portfolio/portfolio-site.png",
     desc: "This is the second version of my portfolio. Keeping the code basic but the site remains interactive done with vanilla JS and CSS.",
-    html: "HTML",
-    css: "CSS",
-    js: "JavaScript",
+    tags: ["HTML", "CSS", "JavaScript"]
   },
   {
     title: 'Movie Watchlist App',
-    demo: "https://joujou144.github.io/movie-watchlist-app/",
-    source: "https://github.com/joujou144/movie-watchlist-app",
+    links: [
+      '<a href="https://github.com/joujou144/movie-watchlist-app" target="_blank">source code</a>',
+      '<a href="https://joujou144.github.io/movie-watchlist-app/" target="_blank">view demo</a>'
+    ],
     image: "./images/portfolio/movie-search-app.png",
     desc: "A 2-page movie watchlist site using JS modules and OMdb API. User can also add and remove favourite movies on a list.",
-    html: "HTML",
-    css: "CSS",
-    api: "API",
-    js: "JavaScript",
+    tags: ["HTML", "CSS", "API", "JavaScript"]
   },
   {
     title: 'Abundent',
-    demo: "https://abundent.com/",
+    links: [
+      '<a href="https://abundent.com/" target="_blank">visit site</a>'
+    ],
     image: "./images/portfolio/Abundent-New-Website.png",
     desc: "Rebranding main website for a Malaysia's IT corporate organisation. Designed by in-house UX team. Completion status - In progress.",
-    html: "HTML",
-    css: "CSS",
-    js: "JavaScript",
+    tags: ["HTML", "CSS", "JavaScript"]
   },
   {
     title: 'Calendar Widget',
-    demo: "https://joujou144.github.io/JS-calendar/",
-    source: "https://github.com/joujou144/JS-calendar",
+    links: [
+      '<a href="https://github.com/joujou144/JS-calendar" target="_blank">source code</a>',
+      '<a href="https://joujou144.github.io/JS-calendar/" target="_blank">view demo</a>'
+    ],
     image: "./images/portfolio/Calendar-Widget.png",
-    desc: "Solo project using CSS and vanilla JS. Calendar dates, month and year display are rendered dynamically using for loops.",
-    html: "HTML",
-    css: "CSS",
-    js: "JavaScript",
+    desc: "Solo project using CSS and vanilla JS. Calendar dates, month and year display are rendered dynamically using JavaScript Date.",
+    tags: ["HTML", "CSS", "JavaScript"]
   },
-
-
+  {
+    title: 'Tenzies Game',
+    links: [
+      '<a href="https://github.com/joujou144/react-tenzies-game" target="_blank">source code</a>'
+    ],
+    image: "./images/portfolio/tenzies.png",
+    desc: "The main features of the game used React fundamentals such as props, useState, useEffect, conditional rendering and event listeners.",
+    tags: ["HTML", "CSS", "React", "JavaScript"]
+  },
+  {
+    title: 'Nokia Snake',
+    links: [
+      '<a href="https://github.com/joujou144/Snake-Game" target="_blank">source code</a>',
+      '<a href="https://joujou144.github.io/Snake-Game/" target="_blank">view demo</a>'
+    ],
+    image: "./images/portfolio/New-Snake.png",
+    desc: "This nostalgic Snake game was re-created with my own styling flair using JavaScript canvas element.",
+    tags: ["HTML", "CSS", "JavaScript"]
+  }
 ]
 
 export { projectData }
